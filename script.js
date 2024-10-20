beb4e11bec1059ed395db2b91107919d3449d021
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 02:23:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-//complete this code
-class Animal {}
+// Define the Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;  // Private property
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound an animal makes
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+// Define the Cat class, extending Animal
+class Cat extends Animal {
+  // Method to log the cat's purring sound
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Define the Dog class, extending Animal
+class Dog extends Animal {
+  // Method to log the dog's barking sound
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
